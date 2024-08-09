5b87eaf324a1f5e8670523c2ce22edc51e627854
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-08-09 17:32:28 +0530

    implemented a big Generic Loader for the entire app using useNavigation hook

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,9 +1,14 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
+import Loader from "./Loader";
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  console.log(navigation);
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
