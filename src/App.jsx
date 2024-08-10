bbc2c9f9099950813f17d7aec668c15ae05daa40
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-08-10 09:00:39 +0530

    initialized empty project for practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,9 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import Home from "./ui/Home";
-import Error from "./ui/Error";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
-import OrderItem from "./features/order/OrderItem";
-import Order, { loader as orderLoader } from "./features/order/Order";
-import AppLayout from "./ui/AppLayout";
-
-const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "menu",
-        element: <Menu />,
-        loader: menuLoader,
-        errorElement: <Error />,
-      },
-      {
-        path: "cart",
-        element: <Cart />,
-      },
-      {
-        path: "/order/new",
-        element: <CreateOrder />,
-      },
-      {
-        path: "/order/:orderId",
-        element: <Order />,
-        loader: orderLoader,
-        errorElement: <Error />,
-      },
-    ],
-  },
-]);
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <div>
+      <h1>Fast React Pizza and Co.</h1>
+    </div>
+  );
 }
 
 export default App;
