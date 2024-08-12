9c4ab89d1bb15adb62ba28b589ef230fda9e3d7f
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-08-12 21:48:07 +0530

    setting up RTK for user

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Dikshant</div>;
+  const user = useSelector((state) => state.user.username);
+
+  if (!user) return null;
+
+  return <div className="hidden text-sm font-semibold md:block">{user}</div>;
 }
 
 export default Username;
