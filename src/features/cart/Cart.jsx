579b21de664d054560516a89a773427f5c4ac42d
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-08-13 16:37:39 +0530

    building cart page

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,35 +1,17 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Button from "../../ui/Button";
 import CartItem from "./CartItem";
+import { clearCart, getCart } from "./cartSlice";
+import EmptyCart from "./EmptyCart";
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
 
 function Cart() {
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
   const username = useSelector((state) => state.user.username);
+  const dispatch = useDispatch();
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="px-4 py-3">
@@ -37,7 +19,7 @@ function Cart() {
         &larr; Back to menu
       </Link>
 
-      <ul className="divide-y divide-stone-200 border-b mt-3">
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
           <CartItem item={item} key={item.pizzaId} />
         ))}
@@ -49,7 +31,9 @@ function Cart() {
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
-        <Button type='secondary'>Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 function EmptyCart() {
   return (
-    <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+    <div className='px-4 py-3'>
+      <Link className='text-blue-400 font-semibold hover:text-blue-300' to="/menu">&larr; Back to menu</Link>
 
-      <p>Your cart is still empty. Start adding some pizzas :)</p>
+      <p className='font-semibold'>Your cart is still empty. Start adding some pizzas :)</p>
     </div>
   );
 }
